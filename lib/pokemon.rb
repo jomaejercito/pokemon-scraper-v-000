1223bc6db81a36ff1862efce24e629429f1c984f
Joma Ejercito <jomaejercito@gmail.com>
2018-03-13 01:32:58 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,6 @@
 class Pokemon
   attr_accessor :id, :name, :type, :hp, :db
 
-
   def initialize(id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
@@ -10,21 +9,16 @@ class Pokemon
     @db = db
   end
 
-
   def self.save(name, type, db)
-    db.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT)")
-    db.execute("INSERT INTO pokemon(name, type) VALUES(?, ?)", [name,type])
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-
-  def self.find(id, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [id]).first
-    self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3] , db: db)
+  def self.find(id_num, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
   end
-
 
   def alter_hp(new_hp, db)
-    db.execute("UPDATE pokemon SET hp = ? WHERE name = ?", [new_hp, self.id])
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
-
 end
