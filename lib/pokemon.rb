0a9f83d41f840ad0bebf351ce3950df83b0e8691
Joma Ejercito <jomaejercito@gmail.com>
2018-03-13 00:30:08 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  @@all = []
+
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+
+  def self.all
+    @@all
+  end
+
+
+  def self.save(name, type, db)
+    db.execute("CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT)")
+    db.execute("INSERT INTO pokemon(name, type) VALUES(?, ?)", [name,type])
+  end
+
+
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [id]).first
+    self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+  end
+
 end
